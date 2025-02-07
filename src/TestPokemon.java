c75dacd1258c8b4f35de611c33de5861e111121e
violetann894 <rachelhuss2976@gmail.com>
2025-02-07 01:02:27 -0500

    Added comments and additional test cases

--- src/TestPokemon.java
+++ src/TestPokemon.java
@@ -1,15 +1,27 @@
+/**
+ * The TestPokemon class tests the Charmander, Pikachu, Pokemon and Stadium classes.
+ * @author Rachel Hussmann
+ */
 public class TestPokemon {
 
     public static void main(String[] args) {
+
+        //Initializing all the objects needed to run a battle
         Charmander charmanderSteve = new Charmander();
-
-        charmanderSteve.getHp();
-
         Stadium tester = new Stadium();
-
         Pikachu ummhmm = new Pikachu();
 
+        //Battle the pokemon
         tester.battle(ummhmm, charmanderSteve);
+
+        System.out.println();
+
+        //Testing the additional constructors
+        Charmander hi = new Charmander(59, 12, 4, 20);
+        Pikachu hello = new Pikachu(70, 10, 6, 40);
+
+        //Battle the pokemon
+        tester.battle(hi, hello);
 
     }
 }
